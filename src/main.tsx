fcc90279ac375d92b74f128777b000f3ad854829
juanpark <juanpark@me.com>
2025-07-21 13:58:22 +0900

    google login implemented with JWT

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log("💡 GOOGLE_CLIENT_ID:", import.meta.env.VITE_GOOGLE_CLIENT_ID);
 createRoot(document.getElementById("root")!).render(<App />);
